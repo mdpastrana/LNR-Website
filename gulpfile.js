b56f03f8515069b3d1b2a9a54a4bbb5938b6a97c
mdpastrana <mdpastrana@straightarrow.com.ph>
2022-07-12 10:48:11 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -55,3 +55,6 @@ function watchTask() {
 
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
